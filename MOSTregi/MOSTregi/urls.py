c9520066548123c3d67021a8b7d359ccec2e9a2c
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-02-12 23:03:24 -0500

    begin creating splash page. Not functional yet.

--- MOSTregi/MOSTregi/urls.py
+++ MOSTregi/MOSTregi/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('register/', include('regform.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
