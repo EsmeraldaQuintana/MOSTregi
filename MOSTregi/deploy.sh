5f27562279163f3c90926a94ba946f9d535a5534
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-02-15 23:46:01 -0500

    refactor deploy script to be less stupid

--- MOSTregi/deploy.sh
+++ MOSTregi/deploy.sh
@@ -1,1 +1,9 @@
-./migrate.sh && ./serve.sh
+#!/bin/zsh
+echo "!!! makemigrations:" &&
+python3 manage.py makemigrations &&
+echo "!!! migrate:" &&
+python3 manage.py migrate &&
+echo "!!! test:" &&
+python3 manage.py test &&
+echo "!!! runserver:" &&
+python3 manage.py runserver
