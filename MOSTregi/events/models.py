8c9f94e08b7303697230f9f87da5044c7f1de819
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-02 07:13:00 -0500

    use django's internal validators to validate fields on forms instead of my janky code

--- MOSTregi/events/models.py
+++ MOSTregi/events/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MaxValueValidator, MinValueValidator, EmailValidator
+
 
 class IntegerRangeField(models.IntegerField):
     def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
@@ -10,15 +12,23 @@ class IntegerRangeField(models.IntegerField):
         defaults.update(kwargs)
         return super(IntegerRangeField, self).formfield(**defaults)
 
+# TO DO: use Django's email validator 
 class registration(models.Model):
     school = models.CharField(max_length=40)
     name = models.CharField(max_length=40)
-    email = models.CharField(max_length=40)
+    email = models.CharField(max_length=40, validators=[EmailValidator])
     telephone = PhoneNumberField() 
     date_request = models.DateTimeField('booking date requested')
     time_request = models.TimeField('booking time requested')
     number_attending = IntegerRangeField(min_value=1, max_value=50)
+    #number_attending = models.IntegerField(
+    #    default = 1, validators=[MaxValueValidator(50), MinValueValidator(1)]
+    #)
     date_time_recieved = models.DateTimeField('date & time booked', auto_now_add=True)
+    def __ls__(self):
+        for field_name in self.__meta.get_all_field_names():
+            value = getattr(self, field_name, None)
+            yield (field_name, value)
 
 # print(registration.__dict__)
 
