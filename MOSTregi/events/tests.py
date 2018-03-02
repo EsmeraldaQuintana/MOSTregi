891902fdee2b9ec73d496e56f08e08ce657d7203
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-02 07:13:40 -0500

    add database, add db_notes.txt, begin writing tests for models

--- MOSTregi/events/tests.py
+++ MOSTregi/events/tests.py
@@ -1,5 +1,22 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 
-# for each django model
+from django.apps import apps
+# from .admin import 
+
+# for each django model, make sure it is registered in ./admin.py
+
+from .models import registration
+
+#class RegistrationModelTest(TestCase):
+#    def test_form_completeness(self):
+#       hasattr(self, ls)
+
+class AdminSiteRegisterTest(TestCase):   
+    app = apps.get_app_config('events')
+    for model in app.get_models():
+        print(model.__name__)
+    #def test_nothing(self):
+       # self.fail("Test not finished...")
