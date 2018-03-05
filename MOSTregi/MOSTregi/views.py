422d5a720e534f385934c7dd42bdd623cb46a922
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-05 16:39:27 -0500

    urls not ending in slash now redirect to same url with a slash..

--- MOSTregi/MOSTregi/views.py
+++ MOSTregi/MOSTregi/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render_to_response
-from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.template import loader, TemplateDoesNotExist
 
 def index(request):
@@ -15,3 +15,13 @@ def fetchHTML(request, title):
         return render_to_response(title)
     except TemplateDoesNotExist:
         raise Http404
+
+def redirect_and_add_slash(request, capture):
+    capture += '/'
+    return redirect(capture)
+
+def testCatch(request, capture):
+    print("========================================")
+    print("=========================================")
+    html = "<html><body> %s </body></html>" % capture
+    return HttpResponse(html)
