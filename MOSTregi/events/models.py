cfea495e451a91fcd263de64d334521eeab49598
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-06 08:06:25 -0500

    registration form now auto adds the current time to field date_time_received

--- MOSTregi/events/models.py
+++ MOSTregi/events/models.py
@@ -1,20 +1,32 @@
 from django.db import models
+from django.db.models import DateTimeField
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator, MinValueValidator, EmailValidator
 
+from django.utils import timezone
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        print("8888888888 datetime.now:", str(datetime.now()))
+        return datetime.now()
+
 class demo_form(models.Model):
     name = models.CharField(max_length=40)
+    date_time_received = AutoDateTimeField('booked on', default=timezone.now)
     date_request = models.DateTimeField('booking date requested')
     number_attending = models.IntegerField(
         default = 1, validators=[MaxValueValidator(50), MinValueValidator(1)]
     )
-    date_time_received = models.DateTimeField('date & time booked', auto_now_add=True)
     def __str__(self):
         return str(self.name) + " " + str(self.date_time_received)
+    #def save(self, *args, **kwargs):
+    #    if not self.id:
+    #        self.date_time_received = timezone.now()
 
 class registration(models.Model):
     school = models.CharField(max_length=40)
     name = models.CharField(max_length=40)
+    date_time_received = AutoDateTimeField('booked on', default=timezone.now)
     email = models.CharField(max_length=40, validators=[EmailValidator])
     telephone = PhoneNumberField() 
     date_request = models.DateTimeField('booking date requested')
@@ -22,9 +34,9 @@ class registration(models.Model):
     number_attending = models.IntegerField(
         default = 1, validators=[MaxValueValidator(50), MinValueValidator(1)]
     )
-    date_time_received = models.DateTimeField('date & time booked', auto_now_add=True)
     def __str__(self):
         return str(self.name) + " " + str(self.date_time_received)
+
 
 # notes:
 # we can do one-to-one, many-to-one, etc using django's ForeignKey field.
