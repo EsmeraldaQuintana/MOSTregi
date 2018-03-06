f342178d37cbbc00d770b52a0f50872712ef0559
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-06 08:24:01 -0500

    add is_upcoming() method to event registration model to see if the event is in the future

--- MOSTregi/events/models.py
+++ MOSTregi/events/models.py
@@ -28,12 +28,15 @@ class registration(models.Model):
     name = models.CharField(max_length=40)
     date_time_received = AutoDateTimeField('booked on', default=timezone.now)
     email = models.CharField(max_length=40, validators=[EmailValidator])
-    telephone = PhoneNumberField() 
-    date_request = models.DateTimeField('booking date requested')
-    time_request = models.TimeField('booking time requested')
+    telephone = PhoneNumberField()
+    date_request = models.DateField('date requested')
+    time_request = models.TimeField('time requested')
     number_attending = models.IntegerField(
         default = 1, validators=[MaxValueValidator(50), MinValueValidator(1)]
     )
+    def is_upcoming(self):
+        date_and_time = datetime.datetime.combine(self.date_request, self.time_request)
+        return date_and_time >= timezone.now()
     def __str__(self):
         return str(self.name) + " " + str(self.date_time_received)
 
