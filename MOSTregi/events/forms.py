9744187a611af762a5ca58b41065c4011de38b68
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-09 15:44:22 -0500

    add date widget to form, format time requested to %H:%M... still trying to get a better format for time input.

--- MOSTregi/events/forms.py
+++ MOSTregi/events/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 from .models import BookingRequest
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
 class BookingRequestForm(forms.ModelForm):
     class Meta:
         model = BookingRequest
@@ -12,3 +18,7 @@ class BookingRequestForm(forms.ModelForm):
                 'time_request',
                 'number_attending',
                 'school')
+        widgets = {
+            'date_request': DateInput(),
+            'time_request': TimeInput(format="%H:%M"),
+        }
