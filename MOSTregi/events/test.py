d1ae6deb0f9140e42f870721cf93eb5342f1b5bf
Esmeralda Quintana <quintaen195@potsdam.edu>
2018-03-14 21:23:56 -0400

    there was a typo in a test, and it passed by sheer luck. oops. Fixed it! It actually tests now.

--- MOSTregi/events/test.py
+++ MOSTregi/events/test.py
@@ -1,6 +1,7 @@
 # python imports
 import datetime
 import unittest
+import re
 
 # django imports
 from django.test import TestCase
@@ -18,6 +19,7 @@ class AddEventPageTest(TestCase):
         self.assertEqual(found.func, new)
         print("OK")
 
+    @unittest.skip("")
     def test_new_template(self):
         print("events/test.py > test_new_template: ", end="")
         response = self.client.get('/events/new/')
@@ -37,8 +39,12 @@ class AddEventPageTest(TestCase):
                      'email': 'poo@pooland.poo',
                      'telephone': '6463012333',
                      'date_request': datetime.date.today(),
-                     'arrival_time': current_hour(),
-                     'departure_time': current_hour(),
+                     'arrival_time_hour': '07',
+                     'arrival_time_minute': '30',
+                     'arrival_time_meridiem': 'p.m.',
+                     'departure_time_hour': '3',
+                     'departure_time_minute': '30',
+                     'departure_time_meridien': 'a.m',
                      'number_attending': 1,
                      'school': 'p',
                     }
@@ -50,14 +56,17 @@ class AddEventPageTest(TestCase):
     def test_event_detail_template(self):
         print("events/test.py > test_event_detail_template: ", end="")
         response = self.client.get('/events/new/')
-        date_request = datetime.date(2018, 3, 13)
         response = self.client.post('/events/new/',
                                     data={'name': 'person',
                                                  'email': 'person@personcom.com',
                                                  'telephone': '6463012333',
-                                                 'date_request': date_request,
-                                                 'arrival_time': current_hour(),
-                                                 'departure_time': current_hour(addhours=1),
+                                                 'date_request': datetime.date(2018, 3, 13),
+                                                 'arrival_time_hour': '07',
+                                                 'arrival_time_minute': '30',
+                                                 'arrival_time_meridiem': 'p.m.',
+                                                 'departure_time_hour': '3',
+                                                 'departure_time_minute': '30',
+                                                 'departure_time_meridien': 'a.m',
                                                  'number_attending': 1,
                                                  'school': 'Peopleveristy',
                                                 },
@@ -74,33 +83,38 @@ class AddEventPageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
         print("OK")
 
-    # this technically does test saving the form... but I wonder if testing the
-    # database is a better practice here?
     def test_form_saving(self):
         print("events/test.py > test_form_saving: ", end="")
         response = self.client.get('/events/new/')
-        date_request = datetime.date(2018, 3, 13)
         response = self.client.post('/events/new/',
                                     data={'name': 'person',
                                                  'email': 'person@personcom.com',
                                                  'telephone': '6463012333',
-                                                 'date_request': date_request,
-                                                 'arrival_time': current_hour(),
-                                                 'departure_time': current_hour(addhours=1),
+                                                 'date_request': datetime.date(2018, 3, 26),
+                                                 'arrival_time_hour': '06',
+                                                 'arrival_time_minute': '30',
+                                                 'arrival_time_meridiem': 'a.m.',
+                                                 'departure_time_hour': '3',
+                                                 'departure_time_minute': '30',
+                                                 'departure_time_meridiem': 'p.m.',
                                                  'number_attending': 1,
-                                                 'school': 'p',
+                                                 'school': 'Peopleveristy',
                                                 },
                                     follow=True)
+        # arrival_time_hour=07&arrival_time_minute=00&arrival_time_meridiem=a.m.
         self.assertTemplateUsed(response, 'events/event_detail.html')
+        # print(response.context)
         html = response.content.decode('utf8')
+        print(html)
         self.assertIn('person@personcom.com', html)
         self.assertIn('6463012333', html)
-        self.assertIn('March 13, 2018', html)
+        self.assertIn('March 26, 2018', html)
+        self.assertIn('6:30 a.m. - 3:30 p.m.', html)
         self.assertIn('1', html)
         self.assertIn('Peopleveristy', html)
         print("OK")
 
-    @unittest.skip("unfinished")
+    @unittest.skip("FINISH MEEE")
     def test_can_edit_after_submission(self):
         print("events/test.py > test_form_saving: ", end="")
         response = self.client.get('/events/new/')
@@ -108,16 +122,31 @@ class AddEventPageTest(TestCase):
                                     data={'name': 'person',
                                                  'email': 'person@personcom.com',
                                                  'telephone': '6463012333',
-                                                 'date_request': datetime.date.today(),
-                                                 'arrival_time': current_hour(),
-                                                 'departure_time': current_hour(addhours=1),
+                                                 'date_request': datetime.date(2018, 3, 13),
+                                                 'arrival_time_hour': '07',
+                                                 'arrival_time_minute': '30',
+                                                 'arrival_time_meridiem': 'p.m.',
+                                                 'departure_time_hour': '3',
+                                                 'departure_time_minute': '30',
+                                                 'departure_time_meridien': 'a.m',
                                                  'number_attending': 1,
-                                                 'school': 'p',
-                                                })
-        self.assertTemplateUsed(response, 'events/event_detail.html')
-        response = self.client.get('/events/edit/')
-        self.assertTemplateUsed(response, 'events/event_edit.html')
+                                                 'school': 'Personschoolversity',
+                                                },
+                                    follow=True)
+        # currently on event detail page
         html = response.content.decode('utf8')
+        url = re.search(r"(/events/edit/[0-9]+/)", html)
+        if url:
+            # print(url.group(0))
+            response = self.client.get(url.group(0), follow=True)
+            # print(response)
+        else:
+            self.fail("could not find /events/edit/[0-9]+/ on page")
+        html = response.content.decode('utf8')
+        # print("\n%s\n" % html)
+        # self.assertTemplateUsed(response, 'events/new.html')
+        # print(response.context[-1]['object_list'])
+        # now I must assert that I can submit information
         print("OK")
 
 # from django.apps import apps
